8e0ca3b705ef6b236aaa22a9bacc616f5a89a5c8
YoyoBLemail <yoelbl2@gmail.com>
2023-03-22 13:26:32 +0200

    added collisions and upgraded design

--- script copy.js
+++ script copy.js
@@ -8,11 +8,14 @@ $activeBodyPart = Array.from($activeBodyPart);
 let intervalId = null;
 let currentDirection = "right";
 let bodyPartHTMLcode = "";
+let pause = null;
+
+let bodyPartPosition = {};
 
 let snakePositions = [
    {
-      x: 25,
-      y: 25,
+      x: 12,
+      y: 12,
    },
 ];
 
@@ -21,7 +24,7 @@ function interval(axis, boolean) {
    snakeMove(axis, boolean);
    intervalId = setInterval(function () {
       snakeMove(axis, boolean);
-   }, 700);
+   }, 200);
 }
 
 //Random fn
@@ -33,20 +36,40 @@ function getRandomInt(min, max) {
 
 //Start game fn
 function startGame() {
-   // debugger;
-
    $playBtn.classList.add("d-none");
 
    bodyPartGenerator();
+
+   intervalId = setInterval(function () {
+      snakeMove("x", true);
+   }, 200);
 }
 
 // Snake move fn
 function snakeMove(axis, positionModifier) {
-   snakePositions.unshift(snakePositions[0]);
+   snakePositions.unshift({ ...snakePositions[0] });
 
    positionModifier
       ? (snakePositions[0][axis] += 1)
       : (snakePositions[0][axis] -= 1);
+
+   checkIfCollision();
+
+   if (
+      bodyPartPosition.x === snakePositions[0].x &&
+      bodyPartPosition.y === snakePositions[0].y
+   ) {
+      $BodyPart[0].classList.remove("body-part");
+      $BodyPart[0].classList.add("active-body-part");
+
+      snakePositions.unshift({ ...snakePositions[0] });
+
+      positionModifier
+         ? (snakePositions[0][axis] += 1)
+         : (snakePositions[0][axis] -= 1);
+
+      bodyPartGenerator();
+   }
 
    for (let i in $activeBodyPart) {
       $activeBodyPart[
@@ -58,6 +81,27 @@ function snakeMove(axis, positionModifier) {
    console.log(snakePositions);
 }
 
+function checkIfCollision() {
+   let checkX = snakePositions[0].x;
+   let checkY = snakePositions[0].y;
+
+   for (let i = 1; i < snakePositions.length; i++) {
+      if (
+         (checkX === snakePositions[i].x && checkY === snakePositions[i].y) ||
+         snakePositions[0].x < 0 ||
+         snakePositions[0].y < 0 ||
+         snakePositions[0].x > 25 ||
+         snakePositions[0].y > 25
+      ) {
+         clearInterval(intervalId);
+         const $gameOver = document.getElementById("game-over");
+
+         $gameOver.classList.remove("d-none");
+         setTimeout(() => window.location.reload(), 1500);
+      }
+   }
+}
+
 window.addEventListener("keydown", function (event) {
    if (currentDirection !== event.code) {
       moveForward(event.code);
@@ -66,46 +110,56 @@ window.addEventListener("keydown", function (event) {
 });
 
 function moveForward(direction) {
-   clearInterval(intervalId);
-   if (direction === "ArrowRight") {
+   if (direction === "ArrowRight" && currentDirection !== "ArrowLeft") {
+      clearInterval(intervalId);
       currentDirection = "ArrowRight";
       interval("x", true);
       return currentDirection;
-   } else if (direction === "ArrowUp") {
+   } else if (direction === "ArrowUp" && currentDirection !== "ArrowDown") {
+      clearInterval(intervalId);
       currentDirection = "ArrowUp";
       interval("y", false);
       return currentDirection;
-   } else if (direction === "ArrowDown") {
+   } else if (direction === "ArrowDown" && currentDirection !== "ArrowUp") {
+      clearInterval(intervalId);
       currentDirection = "ArrowDown";
       interval("y", true);
       return currentDirection;
-   } else if (direction === "ArrowLeft") {
+   } else if (direction === "ArrowLeft" && currentDirection !== "ArrowRight") {
+      clearInterval(intervalId);
       currentDirection = "ArrowLeft";
       interval("x", false);
       return currentDirection;
-   } else {
-      clearInterval(move);
+   } else if (direction === "Space" && !pause) {
+      pause = true;
+      clearInterval(intervalId);
+   } else if (direction === "Space" && pause) {
+      pause = false;
+      moveForward(currentDirection);
    }
 }
 
 function bodyPartGenerator() {
-   let randomPosition = `${getRandomInt(1, 51)} / ${getRandomInt(1, 51)}`;
+   let randomY = getRandomInt(1, 26);
+   let randomX = getRandomInt(1, 26);
 
-   // for (let positions in snakePositions) {
-   //    if (`${positions.y} / ${positions.x}` === randomPosition) {
-   //       return bodyPartGenerator();
-   //    }
-   // }
+   for (let positions of snakePositions) {
+      if (positions.x === randomX && positions.y === randomY) {
+         randomY = getRandomInt(1, 26);
+         randomX = getRandomInt(1, 26);
+      }
+   }
+
+   bodyPartPosition = { x: randomX, y: randomY };
+   let randomPosition = `${randomY} / ${randomX}`;
 
    bodyPartHTMLcode = `<div class="body-part" style="grid-area: ${randomPosition}"></div>`;
    $playArea.innerHTML += bodyPartHTMLcode;
    $BodyPart = document.querySelectorAll(".body-part");
+   $BodyPart = Array.from($BodyPart);
+
    $activeBodyPart = document.querySelectorAll(".active-body-part");
    $activeBodyPart = Array.from($activeBodyPart);
 
    // console.log($BodyPart);
-
-   intervalId = setInterval(function () {
-      snakeMove("x", true);
-   }, 700);
 }
